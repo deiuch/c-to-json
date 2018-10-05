9cde7bd5bdf663d5a5b3bff923a9b7070397cbd1
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-06 00:08:44 +0300

    `ast_builder.c` created.

---
+++ ast_builder.c
@@ -0,0 +1,5 @@
+//
+// Created by Denis Chernikov on 06.10.2018.
+//
+
+#include "ast_builder.h"
---
+++ ast_builder.h
@@ -0,0 +1,8 @@
+//
+// Created by Denis Chernikov on 06.10.2018.
+//
+
+#ifndef C_PARSER_AST_BUILDER_H
+#define C_PARSER_AST_BUILDER_H
+
+#endif //C_PARSER_AST_BUILDER_H
