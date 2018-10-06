552a3cacf6dcb9697fe81942339f57dca4fc7b98
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-06 19:10:51 +0300

    TODO added.

--- main.c
+++ main.c
@@ -5,13 +5,17 @@
  * @authors: Denis Chernikov, Vladislav Kuleykin
  */
 
+#include <stdio.h>
 #include "ast.h"
 #include "y.tab.h"
+
+extern FILE *yyin, *yyout;  // Flex in/out
 
 /// Program entry point.
 int main()
 {
     yydebug = 1;  // Set 0 for no debug info TODO remove in production
+    // TODO set up the input file
 //    astnode *astroot = TODO allocate astroot memory
     if (yyparse(/*astroot*/)) return 1;  // TODO param, error
 //    char *json = asttojson(astroot);  // TODO
