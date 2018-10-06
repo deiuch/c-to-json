adef1f120358b18f621b2c66280cf2e42f333933
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-06 15:21:07 +0300

    Typo fixed :D

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 /// Program entry point.
 int main()
 {
-    yydebug = 1;  // TODO remove in prod, set 0 for no debug info
+    yydebug = 1;  // TODO remove in production, set 0 for no debug info
     // TODO allocate astroot pointer
     if (yyparse(/*astroot*/)) return 1;  // TODO param, error
     // TODO operate on AST
