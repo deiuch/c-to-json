d20f0df33ce805ed5b3586d3845fab4690aed0a8
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-06 20:55:02 +0300

    Program I/O added.

--- main.c
+++ main.c
@@ -12,16 +12,28 @@
 extern FILE *yyin, *yyout;  // Flex in/out
 
 /// Program entry point.
-int main()
+int main(int argc, char *argv[])
 {
     yydebug = 1;  // Set 0 for no debug info TODO remove in production
-//    yyin = fopen( yytext, "r" );
-//    if (!yyin)  /* TODO error */;
+
+    if (argc < 2)
+    {
+        printf("Usage: %s <out_file> OR %s <in_file> <out_file>", argv[0], argv[0]);
+        return 1;
+    }
+
+    yyin = argc > 2 ? fopen(argv[1], "r") : stdin;
+    if (!yyin)  /* TODO error */;
+    FILE *out = argc > 2 ? fopen(argv[2], "w+") : fopen(argv[1], "w+");
+    if (!out) /* TODO error */;
+
 //    astnode *astroot = TODO allocate astroot memory
     if (yyparse(/*astroot*/)) return 1;  // TODO param, error
-//    char *json = ast_to_json(astroot);  // TODO
+    char *json = NULL;
+//    if (json = ast_to_json(astroot)) return 1;  // TODO error
 //    ast_free(astroot);  // TODO
-    // TODO write JSON
-//    fclose(yyin);
+
+    fputs(json, out);
+    if (argc > 2) fclose(yyin);
     return 0;
 }
