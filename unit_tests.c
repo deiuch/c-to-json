9b4b08b243a40cb48d137b8dcee5db07b4513b2b
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-12 23:29:33 +0300

    Unit tests plug added.

---
+++ unit_tests.c
@@ -0,0 +1,14 @@
+/**
+ * Unit tests for parser of the
+ * C Programming Language (ISO/IEC 9899:2018).
+ *
+ * @authors: Denis Chernikov, Vladislav Kuleykin
+ */
+
+#include <assert.h>
+
+int main()
+{
+    assert(1);  // TODO unit tests
+    return 0;
+}
