ef784bc35d16f5fe4b725e77e639affe5e9ac164
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-14 22:01:06 +0300

    `.exe' format removed from the shell script.

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc unit_tests.c alloc_wrap.c ast.c string_tools.c typedef_name.c -o unit_tests.exe
-unit_tests.exe
-rm unit_tests.exe
+gcc unit_tests.c alloc_wrap.c ast.c string_tools.c typedef_name.c -o unit_tests
+unit_tests
+rm unit_tests
 read -p "Press any key to continue . . ."
