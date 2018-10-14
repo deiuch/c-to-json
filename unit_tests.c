a86d670ef335c0e23d3db0fd92ccd6496c7b029f
Denis Chernikov <d.chernikov@innopolis.ru>
2018-10-14 23:32:54 +0300

    Tests for AST_NODE functions added.

--- unit_tests.c
+++ unit_tests.c
@@ -5,12 +5,21 @@
  * @authors: Denis Chernikov, Vladislav Kuleykin
  */
 
-#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include "ast.h"
 #include "string_tools.h"
 #include "typedef_name.h"
+
+/// Conversion function for AST node content.
+/// Copied from `main.c'.
+///
+/// \param obj Object of AST content
+/// \return String representation of the given object, NULL if not AST_NODE
+char *content_to_str(AST_NODE *node)
+{
+    return alloc_const_str((char *) node->content);
+}
 
 int passed = 0;
 int failed = 0;
@@ -47,19 +56,19 @@ int main()
     pass_test(str_eq(repeat(2, NULL), NULL), "repeat(2, NULL)");
 
     // Test `concat_array'
-    pass_test(str_eq(concat_array((char*[]) {"a", "b", "c", "d"}, 4, ", "), "a, b, c, d"),
+    pass_test(str_eq(concat_array((char *[]) {"a", "b", "c", "d"}, 4, ", "), "a, b, c, d"),
         "concat_array((char*[]){\"a\", \"b\", \"c\", \"d\"}, 4, \", \")");
-    pass_test(str_eq(concat_array((char*[]) {"a", "", "c"}, 3, ", "), "a, , c"),
+    pass_test(str_eq(concat_array((char *[]) {"a", "", "c"}, 3, ", "), "a, , c"),
         "concat_array((char*[]){\"a\", \"\", \"c\"}, 3, \", \")");
-    pass_test(str_eq(concat_array((char*[]) {"a", NULL}, 2, ", "), "a, "),
+    pass_test(str_eq(concat_array((char *[]) {"a", NULL}, 2, ", "), "a, "),
         "concat_array((char*[]){\"a\", NULL}, 2, \", \")");
     pass_test(str_eq(concat_array(NULL, 2, ", "), NULL),
          "concat_array(NULL, 2, \", \")");
-    pass_test(str_eq(concat_array((char*[]) {"a", NULL}, -1, ", "), NULL),
+    pass_test(str_eq(concat_array((char *[]) {"a", NULL}, -1, ", "), NULL),
          "concat_array((char*[]){\"a\", NULL}, 2, \", \")");
-    pass_test(str_eq(concat_array((char*[]) {"a", NULL}, 2, NULL), NULL),
+    pass_test(str_eq(concat_array((char *[]) {"a", NULL}, 2, NULL), NULL),
          "concat_array((char*[]){\"a\", NULL}, 2, NULL)");
-    pass_test(str_eq(concat_array((char*[]) {"a"}, 0, ", "), ""),
+    pass_test(str_eq(concat_array((char *[]) {"a"}, 0, ", "), ""),
          "concat_array((char*[]){\"a\", NULL}, 2, NULL)");
 
     // Test `wrap_by_quotes'
@@ -68,8 +77,56 @@ int main()
     pass_test(str_eq(wrap_by_quotes(NULL), NULL), "wrap_by_quotes(NULL)");
     pass_test(str_eq(wrap_by_quotes(""), "\"\""), "wrap_by_quotes(\"\")");
 
-    // TODO typedef: is_typedef_name, put_typedef_name, add_str_typedef.
-    // TODO ast: ast_create_node, ast_expand_node, ast_type_to_str, ast_to_json.
+    // Test `ast_create_node'
+    AST_NODE *node1 = ast_create_node(Identifier, "node1", 0);
+    pass_test(node1->type == Identifier,
+        "ast_create_node(Identifier, \"node1\", 0); node1->type == Identifier;");
+    pass_test(str_eq(node1->content, "node1"),
+        "ast_create_node(Identifier, \"node1\", 0); node1->content == \"node1\";");
+    pass_test(node1->children_number == 0,
+        "ast_create_node(Identifier, \"node1\", 0); node1->children_number == 0;");
+    pass_test(node1->children == NULL,
+        "ast_create_node(Identifier, \"node1\", 0); node1->children == NULL;");
+
+    AST_NODE *node2 = ast_create_node(Identifier, "node2", 1, node1);
+    pass_test(node2->children_number == 1,
+        "ast_create_node(Identifier, \"node2\", 1, node1); node2->children_number == 1;");
+    pass_test(node2->children != NULL,
+        "ast_create_node(Identifier, \"node2\", 1, node1); node2->children != NULL;");
+    pass_test(node2->children[0] == node1,
+        "ast_create_node(Identifier, \"node2\", 1, node1); node2->children[0] == node1;");
+
+    AST_NODE *node3 = ast_create_node(Identifier, "node3", 2, node1, node2);
+    pass_test(node3->children_number == 2,
+        "ast_create_node(Identifier, \"node3\", 2, node1, node2); node3->children_number == 2;");
+    pass_test(node3->children != NULL,
+        "ast_create_node(Identifier, \"node3\", 2, node1, node2); node3->children != NULL;");
+    pass_test(node3->children[0] == node1,
+        "ast_create_node(Identifier, \"node3\", 2, node1, node2); node3->children[0] == node1;");
+    pass_test(node3->children[1] == node2,
+        "ast_create_node(Identifier, \"node3\", 2, node1, node2); node3->children[1] == node2;");
+
+    // Test `ast_expand_node'
+
+    // Test `ast_type_to_str'
+
+    // Test `ast_to_json'
+    char *json1 = "{\n"
+                  "    \"type\": \"Identifier\",\n"
+                  "    \"content\": \"node1\",\n"
+                  "    \"children_number\": 0,\n"
+                  "    \"children\": null\n"
+                  "}";
+    pass_test(str_eq(ast_to_json(node1, 0, "    ", content_to_str), json1),
+        "ast_to_json(node1, 0, \"    \", content_to_str)");
+    char *json2 = "        {\n"
+                  "            \"type\": \"Identifier\",\n"
+                  "            \"content\": \"node1\",\n"
+                  "            \"children_number\": 0,\n"
+                  "            \"children\": null\n"
+                  "        }";
+    pass_test(str_eq(ast_to_json(node1, 2, "    ", content_to_str), json2),
+        "ast_to_json(node1, 2, \"    \", content_to_str)");
 
     printf("\nResults:\n Total number of tests: %d\n Passed: %d\n Failed: %d\n",
         passed + failed, passed, failed);
