a4781a28d257d147542742552c331512a51d902b
Vladislav Kuleykin <kzvdar42@gmail.com>
2018-10-14 23:36:14 +0300

    Added tests for `repeat', `is_typedef_name', `ast_to_str' and `ast_expand_node' from `ast.c'.

--- unit_tests.c
+++ unit_tests.c
@@ -68,8 +68,61 @@ int main()
     pass_test(str_eq(wrap_by_quotes(NULL), NULL), "wrap_by_quotes(NULL)");
     pass_test(str_eq(wrap_by_quotes(""), "\"\""), "wrap_by_quotes(\"\")");
 
-    // TODO typedef: is_typedef_name, put_typedef_name, add_str_typedef.
-    // TODO ast: ast_create_node, ast_expand_node, ast_type_to_str, ast_to_json.
+    // Test `is_typedef_name'
+    put_typedef_name("a");
+    pass_test(is_typedef_name("a"), "put_typedef_name(\"a\");\nis_typedef_name(\"a\")");
+    pass_test(!is_typedef_name("b"), "put_typedef_name(\"a\");\nis_typedef_name(\"b\")");
+    add_std_typedef("math.h");
+    pass_test(is_typedef_name("float_t"), "add_std_typedef(\"math.h\");\nis_typedef_name(\"float_t\")");
+    pass_test(!is_typedef_name("real_t"), "add_std_typedef(\"math.h\");\nis_typedef_name(\"real_t\")");
+
+    // Test `ast_type_to_str'
+    pass_test(str_eq(ast_type_to_str(TranslationUnit), "TranslationUnit"), "ast_type_to_str(TranslationUnit)");
+    pass_test(str_eq(ast_type_to_str(-1),NULL), "ast_type_to_str(-1)");
+
+    //Test `ast_expand_node'
+    AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);
+    AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);
+    AST_NODE* ast_node_expanded = ast_create_node(TranslationUnit, NULL, 0);
+    ast_expand_node(ast_node, ast_node_to_add);
+    pass_test(ast_expand_node(ast_node, ast_node_to_add)->type == ast_node->type,
+         "AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_expanded = ast_expand_node(ast_node, ast_node_to_add);\n"
+         "ast_expand_node(ast_node, ast_node_to_add)->type == ast_node->type");
+    ast_node = ast_create_node(TranslationUnit, NULL, 0);
+    pass_test(ast_expand_node(ast_node, ast_node_to_add)->content == ast_node->content,
+         "AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_expanded = ast_expand_node(ast_node, ast_node_to_add);\n"
+         "ast_expand_node(ast_node, ast_node_to_add)->content == ast_node->content");
+    ast_node = ast_create_node(TranslationUnit, NULL, 0);
+    pass_test(ast_expand_node(ast_node, ast_node_to_add)->children_number == ast_node->children_number,
+         "AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_expanded = ast_expand_node(ast_node, ast_node_to_add);\n"
+         "ast_expand_node(ast_node, ast_node_to_add)->children_number == ast_node->children_number");
+    ast_node = ast_create_node(TranslationUnit, NULL, 0);
+    pass_test(ast_expand_node(ast_node, ast_node_to_add)->children == ast_node->children,
+         "AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_expanded = ast_expand_node(ast_node, ast_node_to_add);\n"
+         "ast_expand_node(ast_node, ast_node_to_add)->children == ast_node->children");
+    pass_test(ast_expand_node(NULL, ast_node_to_add) == NULL,
+         "AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "ast_expand_node(NULL, ast_node_to_add) == NULL");
+    ast_node = ast_create_node(TranslationUnit, NULL, 0);
+    pass_test(ast_expand_node(ast_node, NULL)->children_number == 0,
+         "AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "ast_expand_node(ast_node, NULL)->children_number == 0");
+    ast_node = ast_create_node(TranslationUnit, NULL, 0);
+    pass_test(ast_expand_node(ast_node, NULL)->children == NULL,
+         "AST_NODE* ast_node = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "AST_NODE* ast_node_to_add = ast_create_node(TranslationUnit, NULL, 0);\n"
+         "ast_expand_node(ast_node, NULL)->children == NULL");
+    // TODO ast: ast_create_node, ast_to_json.
 
     printf("\nResults:\n Total number of tests: %d\n Passed: %d\n Failed: %d\n",
         passed + failed, passed, failed);
