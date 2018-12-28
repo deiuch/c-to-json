09af7c2b03915b128b4af66b6f9cc2748f4d7c53
Denchick777 <tchernikoff.denis2013@yandex.ru>
2018-12-28 12:59:17 +0300

    Linux shell script for launching fixed.

--- parse.sh
+++ parse.sh
@@ -2,10 +2,10 @@
 cd src
 bison -y -d parser.y
 flex scanner.l
-gcc main.c lex.yy.c y.tab.c alloc_wrap.c ast.c string_tools.c typedef_name.c -o ../c_parser
+gcc main.c lex.yy.c y.tab.c alloc_wrap.c ast.c literal_conversion.c preprocessing.c string_tools.c typedef_name.c -o ../c_parser
 rm y.tab.c y.tab.h lex.yy.c
 cd ..
 read -p "Enter input filename (leave empty for 'stdin'): " in
-c_parser $in out.txt
+./c_parser $in out.txt
 rm c_parser
 read -p "Press any key to continue . . ."
