94004220e231358c1831483a3a2f137c7a8f9832
Denchick777 <tchernikoff.denis2013@yandex.ru>
2018-12-28 13:12:13 +0300

    Classical Lex introduced; build scripts improved.

--- parse.sh
+++ parse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd src
 bison -y -d parser.y
-flex scanner.l
+flex -l scanner.l
 gcc main.c lex.yy.c y.tab.c alloc_wrap.c ast.c literal_conversion.c preprocessing.c string_tools.c typedef_name.c -o ../c_parser
 rm y.tab.c y.tab.h lex.yy.c
 cd ..
