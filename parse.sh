21d37d950566f39e7cdc0b9236ca7c0c87d19f54
Denchick777 <tchernikoff.denis2013@yandex.ru>
2018-12-28 12:24:04 +0300

    Build & run scripts fixed.

--- parse.sh
+++ parse.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd src
-bison -y -d yacc_syntax.y
-flex flex_tokens.l
-gcc main.c lex.yy.c y.tab.c alloc_wrap.c ast.c string_tools.c typedef_name.c -o c_parser
+bison -y -d parser.y
+flex scanner.l
+gcc main.c lex.yy.c y.tab.c alloc_wrap.c ast.c string_tools.c typedef_name.c -o ../c_parser
 rm y.tab.c y.tab.h lex.yy.c
 cd ..
-read -p "Enter input filename (leave empty for `stdin'): " in
-src/c_parser $in out.txt
-rm src/c_parser
+read -p "Enter input filename (leave empty for 'stdin'): " in
+c_parser $in out.txt
+rm c_parser
 read -p "Press any key to continue . . ."
